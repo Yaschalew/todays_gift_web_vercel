2f9e61a06863e7aacf1eec545e22e599597b01f9
Yaschalew <yaschalewerkihun6@gmail.com>
2024-07-18 04:27:29 -0700

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // this is the default, you can omit this if you haven't changed it
+  }
 })
